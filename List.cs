c3aafd2ca301175f4fec98a400a7d80a2a989b1f
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-08 12:26:40 -0500

    Initial Commit with starter

--- List.cs
+++ List.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace List
 {
-    public class List
+  public class CustomList<T>
+  {
+    private T[] items;
+
+    public CustomList()
     {
+      items = new T[4];
     }
+
+    public void Add(T itemToAdd)
+    {
+
+    }
+  }
 }
