6852f1fccc538f3e676425b89e248bf7ba67e40a
gabrielkunkel <henrysaltandson@gmail.com>
2019-10-08 13:07:10 -0500

    add IEnumerable, indexer (make tests run)

--- List.cs
+++ List.cs
@@ -1,17 +1,32 @@
-﻿namespace List
+﻿using System.Collections;
+
+namespace List
 {
-  public class CustomList<T>
+  public class CustomList<T> : IEnumerable
   {
     private T[] items;
+    public int Count;
+
 
     public CustomList()
     {
       items = new T[4];
     }
 
+    public T this[int i]
+    {
+      get { return items[i]; }
+      set { items[i] = value; }
+    }
+
     public void Add(T itemToAdd)
     {
 
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+      throw new System.NotImplementedException();
     }
   }
 }
